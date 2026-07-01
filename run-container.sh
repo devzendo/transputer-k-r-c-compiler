af14c4773fc5f8a463d8882da34ab653f8e48f15
Matt Gumbley <matt.gumbley@devzendo.org>
2026-07-01 09:00:36 +0100

    Oops.

--- run-container.sh
+++ run-container.sh
@@ -4,4 +4,4 @@ set -euf -o pipefail
 # Starts the 32-bit container to be used for building the compiler, and starts the build.
 UPONE="${PWD%/*}"
 echo Container will have access to $UPONE
-docker run --rm -v /run/host-services/ssh-auth.sock:/ssh-agent -e SSH_AUTH_SOCK="/ssh-agent" -v $UPONE:$UPONE -w $PWD --name transputer-k-r-c-compiler transputer-k-r-c-compiler:latest bash ./build.sh
+docker run --rm -v /run/host-services/ssh-auth.sock:/ssh-agent -e SSH_AUTH_SOCK="/ssh-agent" -v $UPONE:$UPONE -w $PWD --name transputer-k-r-c-compiler transputer-k-r-c-compiler:latest bash ./build-in-container.sh
